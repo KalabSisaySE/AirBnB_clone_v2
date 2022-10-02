b49eed03a0046dd4e21fad46fb6b9cc2bc322aa3
KalabSisaySE <kyimusic11@gmail.com>
2022-10-02 22:36:48 +0300

    fix task-5

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,53 +1,87 @@
 # setups the web servers for deployment
-exec { 'Update Packages':
-  command  => 'sudo apt-get update -y',
-  provider => shell
-}
-exec { 'Install Nginx':
-  command  => 'sudo apt-get install nginx -y',
-  provider => shell,
-  require  => Exec['Update Packages']
-}
-exec { 'Create Shared Folder':
-  command  => 'sudo mkdir -p /data/web_static/shared/',
-  provider => shell,
-  require  => Exec['Install Nginx']
-}
-exec { 'Create Test Folder':
-  command  => 'sudo mkdir -p /data/web_static/releases/test/',
-  provider => shell,
-  require  => Exec['Create Shared Folder']
-}
-exec { 'Create Fake File':
-  command  => 'echo "Holberton School" > /data/web_static/releases/test/index.html',
-  provider => shell,
-  require  => Exec['Create Test Folder'],
-  returns  => [0, 1]
-}
-exec { 'Symbolic Link':
-  command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
-  provider => shell,
-  require  => Exec['Create Fake File']
-}
-exec { 'Give Ownership':
-  command  => 'sudo chown -R ubuntu:ubuntu /data/',
-  provider => shell,
-  require  => Exec['Symbolic Link']
-}
-exec { 'Add Location':
-  command  => 'sudo sed -i "38i\\\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}\n" /etc/nginx/sites-enabled/default',
-  provider => shell,
-  require  => Exec['Give Ownership']
-}
-exec { 'Reload Nginx':
-  command  => 'sudo service nginx reload',
-  provider => shell,
-  require  => Exec['Add Location']
 
-}
-exec { 'Restart Nginx':
-  command  => 'sudo service nginx restart',
-  provider => shell,
-  require  => Exec['Reload Nginx']
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
+
+file { '/data':
+  ensure  => 'directory'
+} ->
+
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
+} ->
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
+}
