12ef99256804bbf25b05916aaeac06210b3a0e15
KalabSisaySE <kyimusic11@gmail.com>
2022-10-02 21:42:58 +0300

    complete task-3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""the `3-deploy_web_static` module
+defines the function `deploy`
+"""
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+do_pack = __import__('1-pack_web_static').do_pack
+
+
+def delpoy():
+    """packs `web_static` and deploys it on the servers"""
+    archive_path = do_pack()
+    if archive_path:
+        return do_deploy(archive_path)
+    else:
+        return False
